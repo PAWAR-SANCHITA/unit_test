13066c639773d38d8d6f6602de66d5ff9bc74a25
PAWAR-SANCHITA <pawarsanchita697@gmail.com>
2024-11-28 15:48:42 +0530

    all good

--- tests/test_helplers.py
+++ tests/test_helplers.py
@@ -1,9 +1,19 @@
+import sys
+import os
+
+# Add the parent directory (unit_testing) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
 import unittest
-from app.helpers import multiply
+from app.helpers import subtract
 
 class TestHelpers(unittest.TestCase):
-    def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
+    def test_subtract(self):
+        self.assertEqual(subtract(2, 1), 1)
+        self.assertEqual(subtract(-3, 1), -4)
+        self.assertEqual(subtract(2, -1), 3)
+        self.assertEqual(subtract(-5, -5), 0)
+        self.assertEqual(subtract(0, 0), 0)
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,21 @@
+import sys
+import os
+
+# Add the parent directory (unit_testing) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+
 import unittest
 from app.main import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-3, -5), -8)
+        self.assertEqual(add(1, -4), -3)
+        self.assertEqual(add(-4, 5), 1)
+        self.assertEqual(add(0, 0), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
