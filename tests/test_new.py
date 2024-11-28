7a814671820b282a5733883acb9ad532ae3e9dd3
PAWAR-SANCHITA <pawarsanchita697@gmail.com>
2024-11-28 16:23:56 +0530

    all right

--- tests/test_new.py
+++ tests/test_new.py
@@ -1,9 +1,10 @@
 import unittest
 from  app.new import shopping_count
-class shopping_count(unittest.TestCase):
 
+class shopping_count(unittest.TestCase):
     def shopping_items(self):
        self.assertEqual(shopping_count(["Dress", "handbag", "soap"]), 3)
        self.assertEqual(shopping_count(["cap", "umbrella"]), 2)
        self.assertEqual(shopping_count([]), 0)
+
 
